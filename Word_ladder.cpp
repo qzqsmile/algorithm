23cb986a5a4d6a88e260e54e02ba08aecfbac741
Q_Q <qzqsmile@gmail.com>
2015-04-16 17:27:43 +0800

    Revert "add"
    This reverts commit b4e0a68a1509d3be7cc89f5e046dd4cf33dce835.

--- Word_ladder.cpp
+++ Word_ladder.cpp
@@ -1,124 +1,53 @@
-//BFS solution, this solution is not effiective
+#include<iostream>
+#include<vector>
+#include<string>
+#include<unordered_set>
+
+using namespace std;
+
 class Solution {
-    public:
-        int ladderLength(string start, string end, unordered_set<string> &dict) {
-           if(dict.empty())
-                return 0;
-           
-           queue<string>q;
-           q.push(start);
-           unordered_map<string, int> visited;
-           unordered_set<string> unvisited = dict;
-           visited[start] = 1;
-           unvisited.erase(start);
-           
-           while(!q.empty())
-           {
-               string word = q.front();
-               q.pop();
-               auto iter = unvisited.begin();
-               while(iter != unvisited.end())
-               {
-                   string adjword = *iter;
-                   if(onediff(word, adjword))
-                   {
-                        visited[adjword] = visited[word] + 1;
-                        if(adjword == end)
-                            return visited[adjword];
-                        iter = unvisited.erase(iter);
-                        q.push(adjword);
-                   }
-                   else
-                        iter++;
-               }
-           }
-            return 0;
-        }
-        
-        inline bool onediff(string &str1, string &str2)
-        {
-            int count = 0;
-            for(int i = 0; i < str1.size(); i++)
-            {
-             if(str1[i] != str2[i])
-             {
-                count++;
-             }
-              if(count > 1)
-                    return false;
-            }
-            return count == 1;
-        }
-    };  
+	public:
+		int ladderLength(string start, string end, unordered_set<string> &dict) {
+			int short_path = dict.size(), flag = 0;
 
-//DBFS solution, this is a effective solution
+			for(auto t = dict.begin(); t != dict.end(); t++)
+			{
+				if(isOneLetter(end, *t))
+				{
+					int store = ladderLength(start, *t, dict);
+					if((short_path > (store + 1)) && ( store != 0))
+					{
+						short_path = store + 1;
+						flag = 1;
+					}
+				}
+			}
 
- /*DBFS method, each time generate all reachable words in the dict that can be transformed from start in the ith steps, and delete this word in dict. 
-Terminate until reach the same word in the other frontier and return dist+1, or a queue is empty and the path does not exist.
-    */
+			if(flag)
+				return short_path;
+			else 
+				return 0;
+		}
 
-    class Solution {
-    public:
-        public:
-            int ladderLength(string start, string end, unordered_set<string> &dict) {
-                queue<string> q1, q2, q3;                        // q1, q2 are the queues for two ends
-                unordered_set<string>   frontier1, frontier2;       //frontier1, frontier2 store words that were just reached on both ends
-                int dist = 1;
-                q1.push(start); q2.push(end);
-                frontier1.insert(start); frontier2.insert(end);
+		bool isOneLetter(string a, string b)
+		{
+			int count = 0;
+			for(int i = 0; i < a.size(); i++)
+			{
+				if(a[i] != b[i])
+					count++;
+			}
+			if(count == 1)
+				return true;
+			return false;
+		}
+};
 
-                while ( !q1.empty() && !q2.empty()) {       //If one queue is empty, no connection can be built
+int main(void)
+{
+	unordered_set<string> dict{"a","b","c"};
+	Solution s;
+	cout << s.ladderLength("a","c",dict) << endl;
+	return 0;
+}
 
-                    if(q1.size() <= q2.size()){
-                        frontier1.clear();
-                        while(!q1.empty()){
-                            string word = q1.front(); q1.pop();
-                            for (int i = 0; i < word.size(); i++) {
-                                string temp = word;
-                                for (char c = 'a'; c <= 'z'; c++) {
-                                    if(c==word[i])  continue;
-                                    temp[i] =c;
-
-                                    if(frontier2.count(temp) > 0)     return dist+1;
-
-                                    if (dict.count(temp) > 0) {
-                                    //use q3 to store the intermediate words that can be reached in the next step
-                                        q3.push(temp);         
-                                        dict.erase(temp);
-                                        frontier1.insert(temp);
-                                    }
-                                }
-                            }
-                        }
-                        dist++;
-                        swap(q1, q3);
-                    }
-                    else{
-                        frontier2.clear();
-                        while(!q2.empty()){
-                            string word = q2.front(); q2.pop();
-                            for (int i = 0; i < word.size(); i++) {
-                                string temp = word;
-                                for (char c = 'a'; c <= 'z'; c++) {
-                                    if(c==word[i])  continue;
-                                    temp[i] =c;
-
-                                    if(frontier1.count(temp) > 0)     return dist+1;
-
-                                    if (dict.count(temp) > 0) {
-                                        //use q3 to store the intermediate words that can be reached in the next step
-                                        q3.push(temp);          
-                                        dict.erase(temp);
-                                        frontier2.insert(temp);
-                                    }
-                                }
-                            }
-                        }
-                        dist++;
-                        swap(q2, q3);
-                    }
-
-                }
-                return 0;
-            }
-    };  
