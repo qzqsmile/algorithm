5f935dc0ca59ab25b6dcef92f56272c719d7fdd8
q <qzqsmile@gmail.com>
2019-10-15 22:00:14 +0800

    add new solution

---
+++ golang/88_merge_sorted_array.go
@@ -0,0 +1,17 @@
+package main
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+	i, j := m-1, n-1;
+	for (i >= 0 && j >= 0){
+		if nums1[i] > nums2[j] {
+			nums1[i+j+1] = nums1[i]
+			i--
+		}else{
+			nums1[i+j+1] = nums2[j]
+			j--
+		}
+	}
+	for ;j >= 0; j--{
+		nums1[j] = nums2[j]
+	}
+}
