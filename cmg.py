a534f762990587f6b894523c8693352be5b30b71
q <qzqsmile@gmail.com>
2020-02-02 11:25:09 +0800

    remove cmg.py

--- cmg.py
+++
@@ -1,8 +0,0 @@
-# import requests
-#
-# res = requests.get(url='https://cmb-game-api.fulugame.cn/systemTime/getCurrentTimes', verify=False)
-
-import requests
-
-res = requests.get('www.baidu.com')
-
